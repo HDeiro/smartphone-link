09a79c35a6ab2e4852af5851c401136c9f519451
Hugo Deiró <hugodeiro@gmail.com>
2024-02-07 18:40:09 -0300

    First Version

---
+++ app.js
@@ -0,0 +1,68 @@
+const $ = document.querySelector.bind(document);
+
+const containers = {
+    insertPhone: $('#insert-phone'),
+    hasPhone:    $('#has-phone'),
+}
+
+const ctas = {
+    getShareableLink: $('#get-shareable-link'),
+    talkOnWhatsApp:   $('#talk-on-whatsapp'),
+    talkOnPhone:      $('#talk-on-phone'),
+    generateNew:      $('#generate-new'),
+}
+
+// Get Query Param
+const url         = new URL(window.location.href);
+const queryParams = new URLSearchParams(url.search);
+const queryParam  = queryParams.get('phone'); // Will be used as flag to know whether we got from qparam or not
+let phoneNumber   = queryParam; // May be null
+
+// Initial rule to define which container will be shown
+containers[queryParam ? 'hasPhone' : 'insertPhone'].classList.remove('none');
+
+// ####################################
+// Generate Trigger
+// ####################################
+
+$('#generate-phone-link').addEventListener('click', ({target}) => {
+    const {value} = target.previousElementSibling;
+
+    if(!value) {
+        alert('Invalid phone number');
+        return;
+    }
+
+    phoneNumber = value;
+
+    containers.insertPhone.classList.add('none');
+    containers.hasPhone.classList.remove('none');
+
+    if (navigator.clipboard) {
+        ctas.getShareableLink.classList.remove('none');   
+    }
+});
+
+// ####################################
+// Use Trigger when Has Phone
+// ####################################
+
+ctas.getShareableLink.addEventListener('click', () => {
+    const shareableLink = `${location.origin}?phone=${phoneNumber}`;
+
+    navigator.clipboard.writeText(shareableLink)
+        .then(() => alert('Shareable link copied to your clipboard'))
+        .catch(() => alert('There was an error on copying your shareable link'));
+});
+
+ctas.talkOnWhatsApp.addEventListener('click', () => window.location.href = `https://api.whatsapp.com/send/?phone=${phoneNumber}`);
+
+ctas.talkOnPhone.addEventListener('click', () => window.location.href = `tel:${phoneNumber}`);
+
+ctas.generateNew.addEventListener('click', () => window.location.href = location.origin);
+
+
+
+
+
+
